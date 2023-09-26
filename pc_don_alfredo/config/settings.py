6a11284ae224e88352484dea5ee8a6046ec8dc45
NicoDS9688 <nicolasdickel@hotmail.com>
2023-09-26 19:12:41 -0400

    Terminado

--- pc_don_alfredo/config/settings.py
+++ pc_don_alfredo/config/settings.py
@@ -143,6 +143,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pcdonalfredo@gmail.com'
 EMAIL_HOST_PASSWORD = 'uhpz niaq tebc cqbr'
-# EMAIL_DESTINO = 'monikstorrere@gmail.com'
-# EMAIL_DESTINO = 'nicolasdickel@gmail.com'
 EMAIL_DESTINO = 'pcdonalfredo@gmail.com'
